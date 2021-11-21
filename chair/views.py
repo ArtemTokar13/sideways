c97b8a01453b3b9a7c7d36608b1da9fcbf6ee2e8
Artem <artem.tokar13@gmail.com>
2021-11-21 19:57:48 +0100

    Add ligin and logout

--- chair/views.py
+++ chair/views.py
@@ -1,4 +1,7 @@
-from django.contrib.auth import login
+import re
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.views import View
 
@@ -8,14 +11,31 @@ from chair.forms import UserRegistrationForm
 class HomeView(View):
 
     def get(self, request):
-        return render(request=request, template_name='chair/homepage.html')
+        form = AuthenticationForm()
+        if request.user.is_authenticated:
+            current_user = request.user.username
+            query_user = re.findall(r'[^\@]+', current_user)
+            speciality = query_user[0]
+            hospital = query_user[1]
+            return render(request=request, template_name='chair/homepage.html',
+                          context={'especialidad': speciality, 'nombre_del_hospital': hospital})
+        return render(request=request, template_name='chair/homepage.html', context={'authentication_form': form})
+
+    def post(self, request):
+        form = AuthenticationForm()
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        return render(request=request, template_name='chair/homepage.html',
+                      context={'authentication_form': form, 'error': 'Los datos son incorrectos. Intenta otra vez por favor.'})
 
 class RegistrationView(View):
     """Registration user view"""
 
     def get(self, request):
         form = UserRegistrationForm()
-        return render(request=request, template_name='chair/registration.html', context={'register_form': form})
+        return render(request=request, template_name='chair/registration.html', context={'registration_form': form})
 
     def post(self, request):
         form = UserRegistrationForm(request.POST)
@@ -24,3 +44,10 @@ class RegistrationView(View):
             login(request, user)
             return redirect('home')
         return render(request=request, template_name='chair/registration.html', context={'registration_form': form})
+
+class LogoutView(View):
+
+    def post(self, request):
+        logout(request)
+        return redirect('home')
+
--- sideways/urls.py
+++ sideways/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from chair.views import RegistrationView, HomeView
+from chair.views import RegistrationView, HomeView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
     path('registration/', RegistrationView.as_view(), name='registration'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    # path('logout/', views.logout, name='logout'),
 ]
